2728e266c1339b70b975f95f787701756c4abd62
jjuyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-10-28 16:37:17 +0900

    [Bronze II] Title: 음계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,16 +1,12 @@
 num = list(map(int, input().split()))
 asc = 0
 des = 0
+asc_arr = sorted(num)
+des_arr = sorted(num, reverse=True)
 
-for i in range(7):
-    if (num[i]+1 == num[i+1]):
-        asc += 1
-    elif (num[i]-1 == num[i+1]):
-        des += 1
-
-if (asc == 7):
+if (num == asc_arr):
     print("ascending")
-elif (des == 7):
+elif (num == des_arr):
     print("descending")
 else:
     print("mixed")
