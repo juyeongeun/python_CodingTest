dba02df8a24c85bc867d31abf5aebc35dcc04849
jjuyeongeun <80696592+juyeongeun@users.noreply.github.com>
2025-02-09 00:06:10 +0900

    [Silver V] Title: 영화감독 숌, Time: 664 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+cnt = 0
+result = 666
+
+while True:
+    if '666' in str(result):
+        cnt += 1
+    if cnt == N:
+        break
+    result += 1
+
+print(result)
