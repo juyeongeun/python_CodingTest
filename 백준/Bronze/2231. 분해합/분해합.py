1a84a747ca24b4d11dbf0ffb52d4eec57b760a94
jjuyeongeun <80696592+juyeongeun@users.noreply.github.com>
2025-02-13 16:06:37 +0900

    [Bronze II] Title: 분해합, Time: 908 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -4,14 +4,7 @@ n = int(sys.stdin.readline())
 num = 0
 
 for i in range(n):
-    tmp = i
-    num = i
-    for j in range(len(str(n))-1, 0, -1):
-        num += tmp//(10**(j))
-        tmp %= 10**j
-    num += i % 10
-
-    if num == n:
+    if i+sum(map(int, str(i))) == n:
         print(i)
         break
 else:
