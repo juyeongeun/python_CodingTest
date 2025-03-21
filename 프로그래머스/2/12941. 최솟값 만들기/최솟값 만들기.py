12b4a672c15d2453b30b6b576f447ea038aaefba
jjuyeongeun <80696592+juyeongeun@users.noreply.github.com>
2025-03-21 15:33:41 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.47 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,8 +1,8 @@
 def solution(A,B):
     answer = 0
     sort_A=sorted(A)
-    sort_B=sorted(B)
+    sort_B=sorted(B, reverse=True)
     for i in range(len(A)):
-        answer+=(sort_A[i]*sort_B[len(A)-i-1])
+        answer+=(sort_A[i]*sort_B[i])
         
     return answer
